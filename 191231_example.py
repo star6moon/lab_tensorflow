66367ebf9549a62ecb02516d651639a67d15d9f3
star6moon <wns913@gmail.com>
2020-01-20 14:59:09 +0900

    Initial commit

---
+++ 191231_example.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+tf.__version__
+
+
+s = [0,1,2,3,4,5,6,7,8,9]
+print(s[3])
---
+++ 200120_tf_mnist_cnn_load_and_save.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+import random
+
+tf.set_random_seed(777)  # reproducibility
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+learning_rate = 0.001
+training_epochs = 3
+batch_size = 100
+
+
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28,1])
+Y = tf.placeholder(tf.float32, [None, 10])
+
+W1 = tf.Variable(tf.random_normal([3,3,1,32], stddev=0.01))
+
+L1 = tf.nn.conv2d(X_img, W1, strides=[1,1,1,1], padding= 'SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1,2,2,1], padding= 'SAME')
+
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1,2,2,1], padding='SAME')
+L2 = tf.reshape(L2, [-1, 7 * 7 * 64])
+
+W3 = tf.get_variable("W2", shape=[7*7*64, 10], initializer=tf.contrib.layers.xavier_initializer())
+b = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L2, W3) + b
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+save_file = './model_0120_1.ckpt' #200120
+saver = tf.train.Saver()
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver.restore(sess, save_file)
+print('training model restored!')
+
+print('Learing started. It takes sometime')
+for epoch in range(training_epochs):
+    ave_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        c, _, = sess.run([cost, optimizer], feed_dict=feed_dict)
+        ave_cost += c / total_batch
+        print('epoch : {} - i : {}'.format(epoch, i))
+    print('Epoch: ', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(ave_cost))
+
+print('Learning Fisnished!')
+
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy: ', sess.run(accuracy, feed_dict = {X: mnist.test.images, Y: mnist.test.labels}))
+
+
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+
+saver.restore(sess, save_file)
+print('training model saved!')
